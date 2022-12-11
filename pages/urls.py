ace88e4bc459a742f034c2c2aa09bfb5d4d29d55
Jean Claude Adams <tech@arrobasoft.net>
2022-12-11 19:39:20 -0300

    start integration

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('checkout', views.checkout, name='checkout')
+    path('checkout', views.checkout, name='checkout'),
+    path('checkout/order-pay/<str:code>', views.order_pay, name='order_pay')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from customers.forms import CustomerForm
+from customers.models import Orders
 from products.models import Product
 
 
@@ -28,3 +29,13 @@ def checkout(request):
     }
 
     return render(request, 'pages/checkout.html', context)
+
+
+def order_pay(request, code):
+    # order = get_object_or_404(Orders, code=code)
+
+    context = {
+        'process_id': 'iZQXZpaQJqkHzZH.9624'
+    }
+
+    return render(request, 'pages/order-pay.html', context)
