fe76acb3c5998cfd1e667cc76c2250e7916aee47
jcroot <tech@arrobasoft.net>
2023-09-01 11:47:37 -0400

    add create product

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Plan, Category, Product, PlanProducts
 from .serializers import PlanSerializer, CategorySerializer, ProductSerializer, PlanProductSerializer
@@ -32,6 +33,14 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Product.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.update(instance, serializer.validated_data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PlanProductViewSet(viewsets.ModelViewSet):
